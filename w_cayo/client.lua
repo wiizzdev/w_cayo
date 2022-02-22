258efa893c3048afa72f8f2c287c63f7938437b5
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-02-22 20:12:01 +0100

    Update client.lua

--- w_cayo/client.lua
+++ w_cayo/client.lua
@@ -5,14 +5,14 @@
 Citizen.CreateThread(function()
     while true do
         local pCoords = GetEntityCoords(GetPlayerPed(-1))        
-            local distance1 = GetDistanceBetweenCoords(pCoords.x, pCoords.y, pCoords.z, 4840.571, -5174.425, 2.0, false)
-            if distance1 < 2000.0 then
+        local distance1 = GetDistanceBetweenCoords(pCoords.x, pCoords.y, pCoords.z, 4840.571, -5174.425, 2.0, false)
+        if distance1 < 2000.0 then
             Citizen.InvokeNative("0x9A9D1BA639675CF1", "HeistIsland", true)  -- charger cayo et enlever los santos
             Citizen.InvokeNative("0x5E1460624D194A38", true) -- charger la minicarte et la carte du menu pause de cayo et enlever celle de los santos
-            else
+        else
             Citizen.InvokeNative("0x9A9D1BA639675CF1", "HeistIsland", false)
             Citizen.InvokeNative("0x5E1460624D194A38", false)
-            end
+        end
         Citizen.Wait(5000)
     end
 end)
